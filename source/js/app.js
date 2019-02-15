2c6e77f6211c27ddaff622582f63d62b508ea6ae
Ruslan-Podvysotsky <podvysutsky00@mail.ru>
2019-02-15 18:55:41 +0200

    doctor

--- source/js/app.js
+++ source/js/app.js
@@ -2827,3 +2827,25 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
 });
+
+$(document).ready(function(){
+	$('.effect__composition').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		infinite: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings:{
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 640,
+				settings:{
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
